7b0f2836f62e5db2f32d5bd4328b6660be57d767
Mutabona <k_dod1@mail.ru>
2024-11-05 21:21:35 +0300

    Add logging to file and console

--- tpo/lr1/lr1/ZadanieTests/StringExtensionTests.cs
+++ tpo/lr1/lr1/ZadanieTests/StringExtensionTests.cs
@@ -1,67 +1,105 @@
-﻿using Zadanie;
+﻿using Serilog;
 using Shouldly;
+using Zadanie;
 
 namespace ZadanieTests;
 
 public class StringExtensionTests
 {
-    [Fact]
-    public void StringExtensionsTest5()
+    private readonly ILogger _logger;
+    private static readonly string LogFilePath = "/home/mutabona/repos/5Semestr/tpo/lr1/lr1/ZadanieTests/StringExtensionsTestsLogs/log.log";
+
+    public StringExtensionTests()
     {
-        var source = ":kukaracha:";
-        
-        var result = source.GetStringBetweenColons();
-        
-        result.ShouldBeEquivalentTo("kukaracha");
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File(LogFilePath, rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        _logger = Log.Logger;
     }
-    
+
     [Fact]
-    public void StringExtensionsTest6()
+    public void StringExtensionsTest1()
     {
-        var source = ":kukaracha:azaza:";
+        var source = "a";
+        var expectedResult = "";
         
         var result = source.GetStringBetweenColons();
         
-        result.ShouldBeEquivalentTo("kukaracha");
+        result.ShouldBeEmpty();
+        
+        var logMessage = $"StringExtensionsTest1 - Source: {source}, Expected result : {expectedResult}, Result: {result}";
+        _logger.Information(logMessage);
+    }
+
+    [Fact]
+    public void StringExtensionsTest2()
+    {
+        var source = ":";
+        var expectedResult = "";
+        
+        var result = source.GetStringBetweenColons();
+        
+        result.ShouldBeEmpty();
+        
+        var logMessage = $"StringExtensionsTest2 - Source: {source}, Expected result : {expectedResult}, Result: {result}";
+        _logger.Information(logMessage);
+    }
+
+    [Fact]
+    public void StringExtensionsTest3()
+    {
+        var source = "abcde";
+        var expectedResult = "";
+        
+        var result = source.GetStringBetweenColons();
+        
+        result.ShouldBeEmpty();
+        
+        var logMessage = $"StringExtensionsTest3 - Source: {source}, Expected result : {expectedResult}, Result: {result}";
+        _logger.Information(logMessage);
     }
     
     [Fact]
     public void StringExtensionsTest4()
     {
         var source = ":kukaracha";
+        var expectedResult = "kukaracha";
         
         var result = source.GetStringBetweenColons();
         
-        result.ShouldBeEquivalentTo("kukaracha");
+        result.ShouldBeEquivalentTo(expectedResult);
+        
+        var logMessage = $"StringExtensionsTest4 - Source: {source}, Expected result : {expectedResult}, Result: {result}";
+        _logger.Information(logMessage);
     }
     
     [Fact]
-    public void StringExtensionsTest3()
+    public void StringExtensionsTest5()
     {
-        var source = "abcde";
+        var source = ":kukaracha:";
+        var expectedResult = "kukaracha";
         
         var result = source.GetStringBetweenColons();
         
-        result.ShouldBeEmpty();
+        result.ShouldBeEquivalentTo(expectedResult);
+        
+        var logMessage = $"StringExtensionsTest5 - Source: {source}, Expected result : {expectedResult}, Result: {result}";
+        _logger.Information(logMessage);
     }
     
     [Fact]
-    public void StringExtensionsTest2()
+    public void StringExtensionsTest6()
     {
-        var source = ":";
+        var source = ":kukaracha:azaza:";
+        var expectedResult = "kukaracha";
         
         var result = source.GetStringBetweenColons();
         
-        result.ShouldBeEmpty();
-    }
-    
-    [Fact]
-    public void StringExtensionsTest1()
-    {
-        var source = "a";
+        result.ShouldBeEquivalentTo(expectedResult);
         
-        var result = source.GetStringBetweenColons();
-        
-        result.ShouldBeEmpty();
+        var logMessage = $"StringExtensionsTest6 - Source: {source}, Expected result : {expectedResult}, Result: {result}";
+        _logger.Information(logMessage);
     }
 }
