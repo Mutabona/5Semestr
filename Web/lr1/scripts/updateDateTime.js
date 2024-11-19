65a353ad2e373a5fb22d6d9f22dc20c5e59fe282
Mutabona <k_dod1@mail.ru>
2024-11-19 11:37:49 +0300

    Add datetime

---
+++ Web/lr1/scripts/updateDateTime.js
@@ -0,0 +1,15 @@
+function updateDateTime() {
+    const now = new Date();
+    const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    const formattedDateTime =
+        ('0' + now.getHours()).slice(-2) + ':' +
+        ('0' + now.getMinutes()).slice(-2) + ':' +
+        ('0' + now.getSeconds()).slice(-2) + ' ' +
+        ('0' + now.getDate()).slice(-2) + '.' +
+        ('0' + (now.getMonth() + 1)).slice(-2) + '.' +
+        now.getFullYear() + ' ' +
+        days[now.getDay()];
+    document.getElementById('datetime').textContent = formattedDateTime;
+}
+setInterval(updateDateTime, 1000);
+updateDateTime();
