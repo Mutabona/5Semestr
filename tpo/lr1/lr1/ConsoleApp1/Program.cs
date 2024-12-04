5ca8a40d5f3b48a6742677af11f3c2ea5d9597fb
Mutabona <k_dod1@mail.ru>
2024-12-05 00:54:50 +0300

    Add lr6 tpo

--- tpo/lr1/lr1/ConsoleApp1/Program.cs
+++ tpo/lr1/lr1/ConsoleApp1/Program.cs
@@ -1,12 +1,23 @@
 ﻿using Zadanie;
 
+var matrix = new MatrixCounter.MatrixCounterBuilder().Build();
+
+var source = new List<List<int>>
+{
+    new List<int> { 1, 2, 3},
+    new List<int> { 4, 5, 6},
+    new List<int> { 7, 8, 9},
+};
+
+var result = matrix.CountNegativeNumbersInRowsWithZeros(source);
+
 var fileNames = new List<string>() {
-    "C:\\Users\\k_dod\\repos\\5Semestr\\tpo\\lr1\\lr1\\ConsoleApp1\\testFile.txt",
-    "C:\\Users\\k_dod\\repos\\5Semestr\\tpo\\lr1\\lr1\\ConsoleApp1\\testFile1.txt",
-    "C:\\Users\\k_dod\\repos\\5Semestr\\tpo\\lr1\\lr1\\ConsoleApp1\\testFile2.txt",
-    "C:\\Users\\k_dod\\repos\\5Semestr\\tpo\\lr1\\lr1\\ConsoleApp1\\testFile3.txt",
-    "C:\\Users\\k_dod\\repos\\5Semestr\\tpo\\lr1\\lr1\\ConsoleApp1\\testFile4.txt",
-    "C:\\Users\\k_dod\\repos\\5Semestr\\tpo\\lr1\\lr1\\ConsoleApp1\\testFile5.txt",
+    @"C:\Users\k_dod\source\repos\5Semestr\tpo\lr1\lr1\ConsoleApp1\testFile.txt",
+    @"C:\Users\k_dod\source\repos\5Semestr\tpo\lr1\lr1\ConsoleApp1\testFile1.txt",
+    @"C:\Users\k_dod\source\repos\5Semestr\tpo\lr1\lr1\ConsoleApp1\testFile2.txt",
+    @"C:\Users\k_dod\source\repos\5Semestr\tpo\lr1\lr1\ConsoleApp1\testFile3.txt",
+    @"C:\Users\k_dod\source\repos\5Semestr\tpo\lr1\lr1\ConsoleApp1\testFile4.txt",
+    @"C:\Users\k_dod\source\repos\5Semestr\tpo\lr1\lr1\ConsoleApp1\testFile5.txt",
         };
 
 var reader = new SadFileReader();
