649bb0fb8fae260d13322545ced2bd122d9c77a9
Mutabona <k_dod1@mail.ru>
2024-12-21 11:25:59 +0300

    Add lr4 net

---
+++ net/lr6/lr6/Footballer.cs
@@ -0,0 +1,7 @@
+﻿namespace lr6;
+
+public sealed class Footballer
+{
+    public string Name { get; set; }
+    public int Goals { get; set; }
+}
---
+++ net/lr6/lr6/Program.cs
@@ -0,0 +1,57 @@
+﻿using lr6;
+
+var footballers = new List<Footballer>
+{
+    new()
+    {
+        Name = "Genadiy",
+        Goals = 10
+    },
+    new()
+    {
+        Name = "Vladimir",
+        Goals = 9
+    },
+    new()
+    {
+        Name = "Svetlana",
+        Goals = 8
+    },
+    new()
+    {
+        Name = "Zinoida",
+        Goals = 0
+    },
+    new()
+    {
+        Name = "Andrey",
+        Goals = 0
+    },
+    new()
+    {
+        Name = "Lexa",
+        Goals = 0
+    },
+};
+
+var bests = footballers
+    .OrderByDescending(x => x.Goals)
+    .Take(3)
+    .ToList();
+    
+var worsts = 
+    from footballer in footballers
+    where footballer.Goals == 0
+    orderby footballer.Goals
+    select footballer;
+    
+Console.WriteLine("Bests: ");
+foreach (var best in bests)
+{
+    Console.WriteLine($"{best.Name}, {best.Goals}");
+}
+Console.WriteLine("Worsts: ");
+foreach (var worst in worsts)
+{
+    Console.WriteLine($"{worst.Name}, {worst.Goals}");
+}
